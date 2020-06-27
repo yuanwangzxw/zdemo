f20a865dde1501b6a4135fa3afd84b799b9c2b1f
zxw <15828024423@163.com>
2020-06-27 23:28:36 +0800

    Jenkinsfile

--- authentication/authentication.sh
+++ authentication/authentication.sh
@@ -2,17 +2,17 @@
 imageName=192.168.56.101:80/test/authentication:0.0.1-SNAPSHOT
 contanierName=authentication
 
-containerId=$(docker ps -a |grep -w ${contanierName}|awk 'print $1')
+containerId=$(docker ps -a |grep -w ${contanierName}|awk '{print $1}')
 if [ -n "$containerId" ]; then
     docker stop ${containerId}
     docker rm ${containerId}
     echo "成功删除容器:$containerId"
 fi
-imageId=$(docker images|grep -w ${imageName} | awk 'print $3')
+imageId=$(docker images|grep -w ${imageName} | awk '{print $3}')
 if [ "$imageId" != "" ]; then
     docker rmi -f ${imageId}
 fi
 docker login -u admin -p Harbor 192.168.56.101:80
 docker pull ${imageName}
 
-docker run -di -p 6001:6001 ${contanierName}
+docker run -di --name ${contanierName} -p 6001:6001 ${imageName}
